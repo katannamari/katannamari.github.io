06e68c84036d83b61daaf5cd321c831f0f81cd0d
Katariina Varanka <n7koka00@students.oamk.fi>
2021-06-27 23:38:47 +0300

    finished cv array

--- script.js
+++ script.js
@@ -62,10 +62,145 @@ const education = [
   },
 ];
 
-let i, len, text;
+const work = [
+  {
+    name: "Riimu Games Oy - 3D-artisti",
+    duration: "Tammikuu 2020 - Kesäkuu 2020",
+    description:
+      "Työharjoittelu. 3D-artistin työt, kuten mallintaminen, teksturointi ja animaatio kahden peliprototyypin kehityksessä",
+  },
+  {
+    name: "Maikkulan kirjasto - Työkokeilu",
+    duration: "Huhtikuu 2013 - Elokuu 2013",
+    description:
+      "Asiakaspalvelu, tietopalvelutehtävät, aineiston hyllyttäminen, muut kirjastossa tehtävät työt",
+  },
+  {
+    name: "Netplaza Oy - Siivooja",
+    duration: "Syyskuu 2012 - Helmikuu 2013",
+    description: "Toimistosiivous",
+  },
+  {
+    name: "Eezy Osk (laskutuspalvelu) - Siivooja",
+    duration: "Helmikuu 2012 - Toukokuu 2012",
+    description:
+      "Matkakeskus Supalle mökkisiivouksia, toimistotöitä ja liinavaatehuoltoa",
+  },
+  {
+    name: "Netplaza Oy - Toimistotyöt",
+    duration: "Maaliskuu 2011 - Elokuu 2012",
+    description:
+      "Keskusaluekarttojen piirtäminen, asiakasrekisterin päivitykset, yritystietojen hakeminen CRM-tietojärjestelmään, taloyhtiökohteiden kiertäminen ja infotaulujen päivittäminen",
+  },
+  {
+    name: "Mantelisilmä ky - Käännöstyöt",
+    duration: "2010",
+    description: "Englannin kielen käännöstöitä",
+  },
+];
 
-for (i = 0, len = education.length, text = ""; i < len; i++) {
-  text +=
+const volunteerWork = [
+  {
+    name: "Nuorten Ystävät",
+    duration: "Syksy 2017 - Kevät 2018",
+    description:
+      "Vapaaehtoistyö Nuorten Ystävien Oulun seudun avopalvelukeskuksessa. Musiikkiryhmän ohjaajana toimiminen yhdessä ryhmän vastuuohjaajien kanssa",
+  },
+];
+
+const contactInfo = [
+  {
+    platform: "Github",
+    url: "github.com/katannamari",
+    faIcon: "fab fa-github",
+  },
+  {
+    platform: "LinkedIn",
+    url: "linkedin.com/in/katariinavaranka",
+    faIcon: "fab fa-linkedin",
+  },
+];
+
+const languages = [
+  {
+    language: "suomi",
+    competency: "äidinkieli",
+  },
+  {
+    language: "englanti",
+    competency: "sujuva",
+  },
+  {
+    language: "ruotsi",
+    competency: "tyydyttävä",
+  },
+];
+
+const skills = [
+  {
+    name: "HTML",
+  },
+  {
+    name: "CSS",
+  },
+  {
+    name: "Bootstrap",
+  },
+  {
+    name: "JavaScript",
+  },
+  {
+    name: "React",
+  },
+  {
+    name: "React Native",
+  },
+  {
+    name: "Blender",
+  },
+  {
+    name: "Krita",
+  },
+  {
+    name: "Figma",
+  },
+];
+
+// Cv Array loop
+let contactMarkup = "";
+let langMarkup = "";
+let skillsMarkup = "";
+let eduMarkup = "";
+let workMarkup = "";
+let volunteerMarkup = "";
+
+// Contact Array
+for (let i = 0; i < contactInfo.length; i++) {
+  contactMarkup +=
+    "<ul class='cv-item'><li>" +
+    contactInfo[i].platform +
+    "</li>" +
+    "<li class='cv-item-duration'>" +
+    contactInfo[i].url +
+    "</li></ul>";
+}
+// Languages Array
+for (let i = 0; i < languages.length; i++) {
+  langMarkup +=
+    "<ul class='cv-item'><li>" +
+    languages[i].language +
+    "</li>" +
+    "<li class='cv-item-description'>" +
+    languages[i].competency +
+    "</li></ul>";
+}
+// Skillset Array
+for (let i = 0; i < skills.length; i++) {
+  skillsMarkup += "<ul class='cv-item'><li>" + skills[i].name + "</li></ul>";
+}
+// Education Array
+for (let i = 0; i < education.length; i++) {
+  eduMarkup +=
     "<ul class='cv-item'><li>" +
     education[i].name +
     "</li>" +
@@ -75,7 +210,37 @@ for (i = 0, len = education.length, text = ""; i < len; i++) {
     "<li class='cv-item-description'>" +
     education[i].description +
     "</li></ul>";
-  console.log(education);
+}
+// Work experience Array
+for (let i = 0; i < work.length; i++) {
+  workMarkup +=
+    "<ul class='cv-item'><li>" +
+    work[i].name +
+    "</li>" +
+    "<li class='cv-item-duration'>" +
+    work[i].duration +
+    "</li>" +
+    "<li class='cv-item-description'>" +
+    work[i].description +
+    "</li></ul>";
+}
+// Volunteer work Array
+for (let i = 0; i < volunteerWork.length; i++) {
+  volunteerMarkup +=
+    "<ul class='cv-item'><li>" +
+    volunteerWork[i].name +
+    "</li>" +
+    "<li class='cv-item-duration'>" +
+    volunteerWork[i].duration +
+    "</li>" +
+    "<li class='cv-item-description'>" +
+    volunteerWork[i].description +
+    "</li></ul>";
 }
 
-document.getElementById("education").innerHTML = text;
+document.getElementById("contact").innerHTML = contactMarkup;
+document.getElementById("languages").innerHTML = langMarkup;
+document.getElementById("skills").innerHTML = skillsMarkup;
+document.getElementById("education").innerHTML = eduMarkup;
+document.getElementById("work").innerHTML = workMarkup;
+document.getElementById("volunteerWork").innerHTML = volunteerMarkup;
