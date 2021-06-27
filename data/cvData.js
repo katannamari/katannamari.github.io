c3efc40953374f846f10ca6edc8d91effdce76d1
Katariina Varanka <n7koka00@students.oamk.fi>
2021-06-27 21:10:27 +0300

    smooth-scroll on navigation

--- data/cvData.js
+++ data/cvData.js
@@ -1,114 +1,127 @@
-export const koulutus = [
+const education = [
   {
-    nimi: "Oulun Ammattikorkeakoulu - Tradenomi (AMK)",
-    kesto: "2017 - 2021",
-    kuvaus: "Tietojenkäsittelyn koulutusohjelma",
+    name: "Oulun Ammattikorkeakoulu - Tradenomi (AMK)",
+    duration: "2017 - 2021",
+    description: "Tietojenkäsittelyn koulutusohjelma",
   },
   {
-    nimi: "Utajärven lukio",
-    kesto: "2007-2010",
-    kuvaus: "Ylioppilastutkinto",
+    name: "Utajärven lukio",
+    duration: "2007-2010",
+    description: "Ylioppilastutkinto",
   },
 ];
+window.localStorage.setItem("education", JSON.stringify(education));
+// export const koulutus = [
+//   {
+//     nimi: "Oulun Ammattikorkeakoulu - Tradenomi (AMK)",
+//     kesto: "2017 - 2021",
+//     kuvaus: "Tietojenkäsittelyn koulutusohjelma",
+//   },
+//   {
+//     nimi: "Utajärven lukio",
+//     kesto: "2007-2010",
+//     kuvaus: "Ylioppilastutkinto",
+//   },
+// ];
 
-export const tyokokemus = [
-  {
-    nimi: "Riimu Games Oy - 3D-artisti",
-    kesto: "Tammikuu 2020 - Kesäkuu 2020",
-    kuvaus:
-      "Työharjoittelu. 3D-artistin työt, kuten mallintaminen, teksturointi ja animaatio kahden peliprototyypin kehityksessä",
-  },
-  {
-    nimi: "Maikkulan kirjasto - Työkokeilu",
-    kesto: "Huhtikuu 2013 - Elokuu 2013",
-    kuvaus:
-      "Asiakaspalvelu, tietopalvelutehtävät, aineiston hyllyttäminen, muut kirjastossa tehtävät työt",
-  },
-  {
-    nimi: "Netplaza Oy - Siivooja",
-    kesto: "Syyskuu 2012 - Helmikuu 2013",
-    kuvaus: "Toimistosiivous",
-  },
-  {
-    nimi: "Eezy Osk (laskutuspalvelu) - Siivooja",
-    kesto: "Helmikuu 2012 - Toukokuu 2012",
-    kuvaus:
-      "Matkakeskus Supalle mökkisiivouksia, toimistotöitä ja liinavaatehuoltoa",
-  },
-  {
-    nimi: "Netplaza Oy - Toimistotyöt",
-    kesto: "Maaliskuu 2011 - Elokuu 2012",
-    kuvaus:
-      "Keskusaluekarttojen piirtäminen, asiakasrekisterin päivitykset, yritystietojen hakeminen CRM-tietojärjestelmään, taloyhtiökohteiden kiertäminen ja infotaulujen päivittäminen",
-  },
-  {
-    nimi: "Mantelisilmä ky - Käännöstyöt",
-    kesto: "2010",
-    kuvaus: "Englannin kielen käännöstöitä",
-  },
-];
+// export const tyokokemus = [
+//   {
+//     nimi: "Riimu Games Oy - 3D-artisti",
+//     kesto: "Tammikuu 2020 - Kesäkuu 2020",
+//     kuvaus:
+//       "Työharjoittelu. 3D-artistin työt, kuten mallintaminen, teksturointi ja animaatio kahden peliprototyypin kehityksessä",
+//   },
+//   {
+//     nimi: "Maikkulan kirjasto - Työkokeilu",
+//     kesto: "Huhtikuu 2013 - Elokuu 2013",
+//     kuvaus:
+//       "Asiakaspalvelu, tietopalvelutehtävät, aineiston hyllyttäminen, muut kirjastossa tehtävät työt",
+//   },
+//   {
+//     nimi: "Netplaza Oy - Siivooja",
+//     kesto: "Syyskuu 2012 - Helmikuu 2013",
+//     kuvaus: "Toimistosiivous",
+//   },
+//   {
+//     nimi: "Eezy Osk (laskutuspalvelu) - Siivooja",
+//     kesto: "Helmikuu 2012 - Toukokuu 2012",
+//     kuvaus:
+//       "Matkakeskus Supalle mökkisiivouksia, toimistotöitä ja liinavaatehuoltoa",
+//   },
+//   {
+//     nimi: "Netplaza Oy - Toimistotyöt",
+//     kesto: "Maaliskuu 2011 - Elokuu 2012",
+//     kuvaus:
+//       "Keskusaluekarttojen piirtäminen, asiakasrekisterin päivitykset, yritystietojen hakeminen CRM-tietojärjestelmään, taloyhtiökohteiden kiertäminen ja infotaulujen päivittäminen",
+//   },
+//   {
+//     nimi: "Mantelisilmä ky - Käännöstyöt",
+//     kesto: "2010",
+//     kuvaus: "Englannin kielen käännöstöitä",
+//   },
+// ];
 
-export const vapaaehtoistyo = [
-  {
-    nimi: "Nuorten Ystävät",
-    kesto: "Syksy 2017 - Kevät 2018",
-    kuvaus:
-      "Vapaaehtoistyö Nuorten Ystävien Oulun seudun avopalvelukeskuksessa. Musiikkiryhmän ohjaajana toimiminen yhdessä ryhmän vastuuohjaajien kanssa",
-  },
-];
+// export const vapaaehtoistyo = [
+//   {
+//     nimi: "Nuorten Ystävät",
+//     kesto: "Syksy 2017 - Kevät 2018",
+//     kuvaus:
+//       "Vapaaehtoistyö Nuorten Ystävien Oulun seudun avopalvelukeskuksessa. Musiikkiryhmän ohjaajana toimiminen yhdessä ryhmän vastuuohjaajien kanssa",
+//   },
+// ];
 
-export const yhteystiedot = [
-  {
-    contactNimi: "Github",
-    url: "github.com/katannamari",
-  },
-  {
-    contactNimi: "LinkedIn",
-    url: "linkedin.com/in/katariinavaranka",
-  },
-];
+// export const yhteystiedot = [
+//   {
+//     contactNimi: "Github",
+//     url: "github.com/katannamari",
+//   },
+//   {
+//     contactNimi: "LinkedIn",
+//     url: "linkedin.com/in/katariinavaranka",
+//   },
+// ];
 
-export const kielitaito = [
-  {
-    kieli: "suomi",
-    taitotaso: "äidinkieli",
-  },
-  {
-    kieli: "englanti",
-    taitotaso: "sujuva",
-  },
-  {
-    kieli: "ruotsi",
-    taitotaso: "tyydyttävä",
-  },
-];
+// export const kielitaito = [
+//   {
+//     kieli: "suomi",
+//     taitotaso: "äidinkieli",
+//   },
+//   {
+//     kieli: "englanti",
+//     taitotaso: "sujuva",
+//   },
+//   {
+//     kieli: "ruotsi",
+//     taitotaso: "tyydyttävä",
+//   },
+// ];
 
-export const osaamisalueet = [
-  {
-    nimi: "HTML",
-  },
-  {
-    nimi: "CSS",
-  },
-  {
-    nimi: "Bootstrap",
-  },
-  {
-    nimi: "JavaScript",
-  },
-  {
-    nimi: "React",
-  },
-  {
-    nimi: "React Native",
-  },
-  {
-    nimi: "Blender",
-  },
-  {
-    nimi: "Krita",
-  },
-  {
-    nimi: "Figma",
-  },
-];
+// export const osaamisalueet = [
+//   {
+//     nimi: "HTML",
+//   },
+//   {
+//     nimi: "CSS",
+//   },
+//   {
+//     nimi: "Bootstrap",
+//   },
+//   {
+//     nimi: "JavaScript",
+//   },
+//   {
+//     nimi: "React",
+//   },
+//   {
+//     nimi: "React Native",
+//   },
+//   {
+//     nimi: "Blender",
+//   },
+//   {
+//     nimi: "Krita",
+//   },
+//   {
+//     nimi: "Figma",
+//   },
+// ];
